c7dcd09672683cd96acd126d8b9faea6bd5f115c
SaiTejaKolagani <saiteja.kolagani@gmail.com>
2024-08-24 19:29:17 +0530

    styled not found page

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -8,8 +8,8 @@ const NotFound = ({ theme }) => {
   return (
     <div className="not-found-container">
       <h1 style={styleHeading}>404 - Page Not Found</h1>
-      <p className='animate-ping' style={styleHeading}>Sorry, the page you're looking for doesn't exist.</p>
-      <Link to="/" className="not-found-link" style={styleHeading}><ArrowBackIcon  />Go Back to Home</Link>
+      <p style={styleHeading}>Sorry, the page you're looking for doesn't exist.</p>
+      <Link to="/" className="not-found-link" style={styleHeading}><ArrowBackIcon className='animate-ping' />Go Back to Home</Link>
     </div>
   );
 };
