5fabbca0a0cd8f96fd8d4fde90b42398222f1f45
SaiTejaKolagani <saiteja.kolagani@gmail.com>
2024-08-24 17:39:06 +0530

    Adjusted Sign Up form

--- src/components/PostDetail/index.js
+++ src/components/PostDetail/index.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
+import { ThreeDots } from 'react-loader-spinner';
 import EditIcon from '@mui/icons-material/Edit';
 import { API_URL } from '../../data/apiPath';
 
@@ -8,23 +9,33 @@ import './index.css'
 const PostDetail = ({theme}) => {
   const { id } = useParams();
   const [post, setPost] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetch(`${API_URL}/api/posts/${id}`)
       .then(response => response.json())
       .then(data => {
         setPost(data);
+        setLoading(false);
       })
       .catch(error => {
         console.error('Error fetching post details:', error);
+        setLoading(false);
       });
   }, [id]);
-
-  if (!post) return <p>Loading...</p>;
 
   const styleTitle = theme ? null : { color: "#e372ff" };
   const styleContent = theme ? null : {color: "#f3ceff"}
   const styleDetails = theme ? null : {color: "#eda7ff"}
+  const styleHeading = theme ? null : { color: "#e372ff" };
+
+  if (loading) {
+    return (
+      <div className="loader-container" style={styleHeading}>
+        <ThreeDots color="#470052" height={50} width={50} />
+      </div>
+    );
+  }
 
   return (
     <div className='post-detail-container'>
